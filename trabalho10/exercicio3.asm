11afe2a8af26d286a345df8075ec119d87686c0c
Lorenzo F. Antunes <fantunes.lorenzo@gmail.com>
2016-06-16 09:34:41 -0300

    sl

--- trabalho10/exercicio3.asm
+++ trabalho10/exercicio3.asm
@@ -1,29 +1,55 @@
 .data
-    frase: .ascii "ONE RING to rule them aLL"
+    #frase: .ascii "ONE RING to rule them aLL\0"
+    frase: .ascii " OnE\0"
 .text
     #$t0 ponteiro da frase
     #$t1 const 20
-    #$t2 const 65
-    #$t3 const 97
-    #t4 letra
+    #$t2 const 64
+    #$t3 const 91
+    #$t4 const 96
+    #$t5 const 123
+    #$t6 letra
+    #$t7 flag 1
+    #$t8 flag 2
     la $t0, frase
+
     #define constantes
-    li $t1, 20
-    li $t2, 65
-    li $t3, 97
+    li $t1, 32
+    li $t2, 64
+    li $t3, 91
+    li $t4, 96
+    li $t5, 123
 
     loop:
-        lbu $t4, ($t0) #carrega a letra
+        lbu $t6, ($t0) #carrega a letra
 
-        beq $t4, $zero, fim #ve se acabou
+        beq $t6, $zero, fimLoop #ve se acabou
+        nop
+        beq $t6, $t1, incrementa #ve se é espaco
         nop
 
-        beq $t4, $t1, incrementa #ve se é espaco
+        slt $t7, $t2, $t6 # se a letra for maior que 64 seta 1
+        slt $t8, $t6, $t3 # se a letra for menor que 91 seta 1
+        and $t7, $t7, $t8 #and entre elas
+        not $t7, $t7  #invert valor
+
+        beq $t7 $zero, minimiza
         nop
+
+        #beq ???, $zero, capitalize
+        #nop
+
 
         incrementa:
         addi $t0, $t0, 1 #incrementa a letra
         j loop
+    nop
+    fimLoop:
+
+    #imprime a frase
+    la $a0, frase
+    li $v0, 4
+    syscall
     nop
 
     j fim
@@ -31,11 +57,11 @@
 
 ################################################
     minimiza:
-
+    #
     jr $ra
     nop
 
-    capi:
+    capitalize:
 
     jr $ra
     nop
